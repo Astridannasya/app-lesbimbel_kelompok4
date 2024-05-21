3dd80d1ab655a580dc0fcacefa17205654c62fce
Astridannasya <astridannasyaputri@gmail.com>
2024-05-21 20:25:11 +0700

    menambahkan admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+//route menggunakan get berarti mengambil fungsi didalam controller
+//route mengginakan resources berarti mengambil class
+//route dengan menggunakan name atau by name
+
+Route ::get('/admin', [AdminController::class, 'index']);
+
